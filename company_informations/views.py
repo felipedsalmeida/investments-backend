df26041085d7082658a76bf6b93a7f718ea2edcb
cadusds <carlos.silva150995@gmail.com>
2021-08-18 23:30:13 -0300

    comment views

--- company_informations/views.py
+++ company_informations/views.py
@@ -1,9 +1,9 @@
 from django.utils import timezone
-from django.views.generic.list import ListView
-import fundamentus
+# from django.views.generic.list import ListView
+# import fundamentus
 
-from company_informations.models import Sectors
+# from company_informations.models import Sectors
 
-class SectorsListView(ListView):
-    model=Sectors
-    paginate_by=20 # if pagination is desired
+# class SectorsListView(ListView):
+#     model=Sectors
+#     paginate_by=20 # if pagination is desired
